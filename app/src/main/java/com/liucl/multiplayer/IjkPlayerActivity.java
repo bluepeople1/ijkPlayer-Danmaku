024dd84858dfabca39888693e85d0bcc73014b5d
liucloo <aabbcclcl@hotmail.com>
2015-11-11 19:50:10 +0800

    2015年11月11日19:50:02

--- app/src/main/java/com/liucl/multiplayer/IjkPlayerActivity.java
+++ app/src/main/java/com/liucl/multiplayer/IjkPlayerActivity.java
@@ -1,58 +1,32 @@
 package com.liucl.multiplayer;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
 
 import com.liucl.multiplayer.widget.media.IjkVideoView;
 
-import tv.danmaku.ijk.media.player.IMediaPlayer;
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 /**
  * ijkPlayer
  * Created by 刘晨龙 on 2015/11/10.
  */
-public class IjkPlayerActivity extends AppCompatActivity {
-
-    private IjkVideoView mVideoView;
+public class IjkPlayerActivity extends PlayerActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ijk_player);
-        Uri[] uri = getUri();
-
-        // init player
-        IjkMediaPlayer.loadLibrariesOnce(null);
-        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
-
-        mVideoView = (IjkVideoView) findViewById(R.id.videoview);
-//        mVideoView.setHudView(mHudView);
-        // prefer mVideoPath
-        mVideoView.setVideoPath(uri[0].toString());
-        mVideoView.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(IMediaPlayer mp) {
-                mp.start();
-            }
-        });
-        mVideoView.start();
-
+    protected int getContentView() {
+        return R.layout.activity_ijk_player;
     }
 
+    @Override
+    protected void assignViews() {
+        super.assignViews();
+        mVideoView = (IjkVideoView) findViewById(R.id.videoview);
+    }
 
-    /**
-     * 获取Uri
-     * 1、视频地址
-     * 2、弹幕地址
-     * @return
-     */
-    public Uri[] getUri() {
-        Uri[] url = new Uri[2];
-        url[0] = Uri.parse(getIntent().getStringExtra("videoPath"));
-        url[1] = Uri.parse(getIntent().getStringExtra("danmakuPath"));
-        return url;
+    @Override
+    protected void configSystem() {
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
     }
 
 }
