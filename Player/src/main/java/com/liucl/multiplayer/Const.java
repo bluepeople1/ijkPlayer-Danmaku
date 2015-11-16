81731756b6ebb05b813179e596239d024d103b02
liucloo <aabbcclcl@hotmail.com>
2015-11-16 08:48:25 +0800

    修复一些bug

---
+++ Player/src/main/java/com/liucl/multiplayer/Const.java
@@ -0,0 +1,8 @@
+package com.liucl.multiplayer;
+
+/**
+ * Created by 刘晨龙 on 2015/11/10.
+ */
+public interface Const {
+    String videoUrl = "http://192.168.1.120:8080/videotest/video/a.wmv";
+}
--- app/src/main/java/com/liucl/multiplayer/Const.java
+++
@@ -1,8 +0,0 @@
-package com.liucl.multiplayer;
-
-/**
- * Created by 刘晨龙 on 2015/11/10.
- */
-public interface Const {
-    String videoUrl = "http://ws.acgvideo.com/4/10/4955124-1hd.mp4?wsTime=1447461661&wsSecret2=8f752714ea645651647b3bbc735e943a&oi=2099736579&appkey=5256c25b71989747&or=2099709835";
-}
